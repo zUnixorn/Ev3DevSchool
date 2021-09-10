3c910532908f83186a4841d61a9ae08aebdb4a77
flareflo <florianlipp17@gmail.com>
2021-09-10 11:45:33 +0200

    Testing ultrasonic

--- src/main/java/experiment/ExperimentMain.java
+++ src/main/java/experiment/ExperimentMain.java
@@ -1,8 +1,14 @@
 package experiment;
 
+import ev3dev.actuators.Sound;
+import ev3dev.actuators.ev3.EV3Led;
 import ev3dev.actuators.lego.motors.EV3LargeRegulatedMotor;
 import ev3dev.sensors.Battery;
+import ev3dev.sensors.ev3.EV3ColorSensor;
+import ev3dev.sensors.ev3.EV3TouchSensor;
+import ev3dev.sensors.ev3.EV3UltrasonicSensor;
 import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
 import lejos.utility.Delay;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,11 +19,14 @@ public class ExperimentMain {
 
     public static int DELAY = 2000;
 
-    public static final EV3LargeRegulatedMotor mA = new EV3LargeRegulatedMotor(MotorPort.A);
+//    public static final EV3LargeRegulatedMotor mA = new EV3LargeRegulatedMotor(MotorPort.A);
 
     public static void main(String[] args) {
         final EV3LargeRegulatedMotor motorLeft = new EV3LargeRegulatedMotor(MotorPort.A);
         final EV3LargeRegulatedMotor motorRight = new EV3LargeRegulatedMotor(MotorPort.B);
+        final EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S3);
+        final EV3UltrasonicSensor ultrasonicSensor = new EV3UltrasonicSensor(SensorPort.S2);
+        final EV3TouchSensor touchSensor = new EV3TouchSensor(SensorPort.S1);
 
         Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
             public void run() {
@@ -27,38 +36,11 @@ public class ExperimentMain {
             }
         }));
 
-        System.out.println("Defining the Stop mode");
-        motorLeft.brake();
-        motorRight.brake();
+        ultrasonicSensor.enable();
 
-        System.out.println("Defining motor speed");
-        final int motorSpeed = 200;
-        motorLeft.setSpeed(motorSpeed);
-        motorRight.setSpeed(motorSpeed);
-
-        System.out.println("Go Forward with the motors");
-        motorLeft.forward();
-        motorRight.forward();
-
-        Delay.msDelay(2000);
-
-        System.out.println("Stop motors");
-        motorLeft.stop();
-        motorRight.stop();
-
-        System.out.println("Go Backward with the motors");
-        motorLeft.backward();
-        motorRight.backward();
-
-        Delay.msDelay(2000);
-
-        System.out.println("Stop motors");
-        motorLeft.stop();
-        motorRight.stop();
-
-        System.out.println("Checking Battery");
-        System.out.println("Voltage: " + Battery.getInstance().getVoltage());
-
-        System.exit(0);
+       while (true) {
+           System.out.println(ultrasonicSensor);
+       }
+//        System.exit(0);
     }
 }
